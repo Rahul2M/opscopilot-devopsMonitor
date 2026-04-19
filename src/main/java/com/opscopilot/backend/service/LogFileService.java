fff3ef778784735aab984a0131a798f03cd51ba8
Artist <rp1682737@gmail.com>
2026-04-19 12:28:09 +0530

    feat: log file integration + UI support

--- src/main/java/com/opscopilot/backend/service/LogFileService.java
+++ src/main/java/com/opscopilot/backend/service/LogFileService.java
@@ -1,30 +1,23 @@
 package com.opscopilot.backend.service;
 
-
-
-
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Service
 public class LogFileService {
 
     public List<String> readLogs() {
-
         try {
-            InputStream input = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("logs/sample.log");
+            ClassPathResource resource = new ClassPathResource("logs/sample.log");
 
-            if (input == null) {
-                throw new RuntimeException("Log file not found in resources!");
-            }
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(resource.getInputStream())
+            );
 
             return reader.lines().collect(Collectors.toList());
 
